0326f52465c0b0f7e34f9a38158a4175e0bcff8c
fourcolor <james1qaz2wsx12qw@gmail.com>
2024-12-05 14:06:10 +0800

    Run mobileinsight in main process

--- runner.py
+++ runner.py
@@ -8,14 +8,21 @@ import json
 from datetime import datetime as dt
 from utils.myMsgLogger import MyMsgLogger
 
-def get_ser(folder, dev):
+
+def get_ser(folder, dev: str):
     d2s_path = os.path.join(folder, "device_setting.json")
     with open(d2s_path, "r") as f:
         device_to_serial = json.load(f)
-        return os.path.join(
-            "/dev/serial/by-id",
-            f"usb-SAMSUNG_SAMSUNG_Android_{device_to_serial[dev]}-if00-port0",
-        )
+        if dev.startswith("sm"):
+            return os.path.join(
+                "/dev/serial/by-id",
+                f"usb-SAMSUNG_SAMSUNG_Android_{device_to_serial[dev]}-if00-port0",
+            )
+        else:
+            return os.path.join(
+                "/dev/serial/by-id",
+                f"usb-Quectel_RM500Q-GL_{device_to_serial[dev]}-if00-port0",
+            )
 
 
 class LoopTimer(Thread):
@@ -55,7 +62,7 @@ class Runner:
         self.src = OnlineMonitor()
         self.src.set_serial_port(get_ser(os.path.dirname(__file__), dev))
         self.src.set_baudrate(baudrate)
-        
+
         self.feature_extractor = feature_extractor
         self.feature_extractor.set_source(self.src)
         self.predictor = predictor
@@ -63,33 +70,45 @@ class Runner:
 
         self.pred_task = LoopTimer(predict_interval, self.predict_task)
         self.main_task = Thread(target=self.run_task, daemon=True)
-        
-        self.log_dir = self.create_log_dir()
-        self.mi2log_log_path = os.path.join(self.log_dir, "diag_log_{}_{}.mi2log".format(dev, os.path.basename(self.log_dir)))
-        self.xml_log_path = os.path.join(self.log_dir, "diag_log_{}_{}.xml".format(dev, os.path.basename(self.log_dir)))
-        
+
+        self.log_dir = self.create_log_dir(log_dir)
+        self.mi2log_log_path = os.path.join(
+            self.log_dir,
+            "diag_log_{}_{}.mi2log".format(dev, os.path.basename(self.log_dir)),
+        )
+        self.xml_log_path = os.path.join(
+            self.log_dir,
+            "diag_log_{}_{}.xml".format(dev, os.path.basename(self.log_dir)),
+        )
+
         dumper = MyMsgLogger()
         dumper.set_source(self.src)
         dumper.set_decoding(MyMsgLogger.XML)  # decode the message as xml
         dumper.set_dump_type(MyMsgLogger.ALL)
         dumper.save_decoded_msg_as(self.xml_log_path)
         self.src.save_log_as(self.mi2log_log_path)
-    
+
     def create_log_dir(self, log_dir):
         if log_dir is None:
             now = dt.today()
-            n = [str(x) for x in [now.year, now.month, now.day, now.hour, now.minute, now.second]]
-            n = [x.zfill(2) for x in n] 
-            n = '-'.join(n[:3]) + '_' + '-'.join(n[3:])
+            n = [
+                now.year,
+                now.month,
+                now.day,
+                now.hour,
+                now.minute,
+                now.second,
+            ]
+            n = [str(x).zfill(2) for x in n]
+            n = "-".join(n[:3]) + "_" + "-".join(n[3:])
             os.umask(0)
-            log_dir = os.path.join(os.path.dirname(__file__), 'log', str(n))
+            log_dir = os.path.join(os.path.dirname(__file__), "log", str(n))
         os.makedirs(log_dir, exist_ok=True)
         return log_dir
 
-
     def run(self):
-        self.main_task.start()
         self.pred_task.start()
+        self.src.run()
         # print('task running', flush=True)
 
     def run_task(self):
@@ -97,7 +116,6 @@ class Runner:
 
     def predict_task(self):
         x_in = self.feature_extractor.get_feature_dict()
-        # print("predict", flush=True)
         pred_output = self.predictor.predict(x_in)
         self.actor.do_action(pred_output)
 
@@ -123,7 +141,7 @@ if __name__ == "__main__":
     nr_ss_extractor = NR_Signal_Strength_Extractor()
     nr_ss_extractor.set_source_parser(nr_ss_parser)
 
-    feature_extractor = FeatureExtractor(offline=True)
+    feature_extractor = FeatureExtractor()
     feature_extractor.add_parser(rrc_ota_parser)
     feature_extractor.add_parser(lte_ss_parser)
     feature_extractor.add_parser(nr_ss_parser)
@@ -138,13 +156,13 @@ if __name__ == "__main__":
         actor=Actor(),
         dev="sm01",
     )
-    
+
     try:
-    
+
         runner.run()
         while True:
             time.sleep(1)
     except Exception as e:
         import traceback
+
         traceback.print_exc()
-        
